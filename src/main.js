3d8dc2b81c51030304a07600945466063311bc73
小蜗牛 <asln_lz@126.com>
2018-04-20 10:45:33 +0800

    w

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import iView from 'iview'
-import 'iview/dist/styles/iview.css';
 import '../lib/font-awesome.min.css';
 import '../lib/index.css'
 
 Vue.config.productionTip = false
-Vue.use(iView);
+
 
 /* eslint-disable no-new */
 new Vue({
